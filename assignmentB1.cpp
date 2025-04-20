b559d73060851d51b6b6f0d0d0b88c736262257d
imshubhamvs <139503231+imshubhamvs@users.noreply.github.com>
2025-04-21 00:30:26 +0530

    Update assignmentB1.cpp

--- assignmentB1.cpp
+++ assignmentB1.cpp
@@ -1,106 +1,66 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Queens {
-private:
-    int size;
-    int **board;
+const int MAX = 20; // max board size
 
-public:
-    Queens(int n) {
-        size = n;
-        board = new int *[n];
-        for (int i = 0; i < n; i++) {
-            board[i] = new int[n];
-            fill(board[i], board[i] + n, 0);
+int board[MAX][MAX]; // 2D board array
+
+bool isSafe(int row, int col, int n) {
+    // Check this column in previous rows
+    for (int i = 0; i < row; i++)
+        if (board[i][col]) return false;
+
+    // Check upper left diagonal
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j]) return false;
+
+    // Check upper right diagonal
+    for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+        if (board[i][j]) return false;
+
+    return true;
+}
+
+bool solveNQueens(int row, int n) {
+    if (row == n) return true;
+
+    for (int col = 0; col < n; col++) {
+        if (isSafe(row, col, n)) {
+            board[row][col] = 1;
+            if (solveNQueens(row + 1, n)) return true;
+            board[row][col] = 0; // backtrack
         }
     }
+    return false;
+}
 
-    ~Queens() {
-        for (int i = 0; i < size; i++) {
-            delete[] board[i];
+void printBoard(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "|";
+            if (board[i][j]) cout << "X";
+            else cout << " ";
         }
-        delete[] board;
+        cout << "|" << endl;
     }
-
-    void do_visit(int row, int column) {
-        for (int i = 0; i < size; i++) {
-            board[row][i]++;
-            board[i][column]++;
-        }
-
-        // diagonals
-        for (int i = row, j = column; i < size && j < size; i++, j++)
-            board[i][j]++;
-        for (int i = row, j = column; i >= 0 && j >= 0; i--, j--)
-            board[i][j]++;
-        for (int i = row, j = column; i < size && j >= 0; i++, j--)
-            board[i][j]++;
-        for (int i = row, j = column; i >= 0 && j < size; i--, j++)
-            board[i][j]++;
-
-        // Correct for overcounting the (row, column) cell
-        board[row][column] -= 5;
-    }
-
-    void undo_visit(int row, int column) {
-        for (int i = 0; i < size; i++) {
-            board[row][i]--;
-            board[i][column]--;
-        }
-
-        // diagonals
-        for (int i = row, j = column; i < size && j < size; i++, j++)
-            board[i][j]--;
-        for (int i = row, j = column; i >= 0 && j >= 0; i--, j--)
-            board[i][j]--;
-        for (int i = row, j = column; i < size && j >= 0; i++, j--)
-            board[i][j]--;
-        for (int i = row, j = column; i >= 0 && j < size; i--, j++)
-            board[i][j]--;
-
-        // Correct for over-removing the (row, column) cell
-        board[row][column] += 5;
-    }
-
-    bool Nqueens(int curr_row) {
-        if (curr_row == size)
-            return true;
-
-        for (int i = 0; i < size; i++) {
-            if (board[curr_row][i] == 0) {
-                do_visit(curr_row, i);
-                board[curr_row][i] = -1;  // Place queen
-                if (Nqueens(curr_row + 1))
-                    return true;
-                board[curr_row][i] = 0;   // Remove queen
-                undo_visit(curr_row, i);
-            }
-        }
-        return false;
-    }
-
-    void solve() {
-        if (Nqueens(0)) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (board[i][j] == -1) {
-                        cout << "(" << i << "," << j << "), ";
-                    }
-                }
-            }
-            cout << endl;
-        } else {
-            cout << "No solution exists.\n";
-        }
-    }
-};
+}
 
 int main() {
     int n;
     cout << "Enter number of queens: ";
     cin >> n;
-    Queens q(n);
-    q.solve();
+
+    // Initialize board
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            board[i][j] = 0;
+
+    if (solveNQueens(0, n)) {
+        cout << "\nSolution:\n";
+        printBoard(n);
+    } else {
+        cout << "No solution exists for " << n << " queens.\n";
+    }
+
     return 0;
 }
